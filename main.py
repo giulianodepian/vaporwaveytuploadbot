c19cb332ecd7bebc71caed30a946645b4ff77f93
Giuliano De Pian <giulianodepian97@gmail.con>
2025-06-30 18:16:03 -0300

    Added YT Uploader
    Avoid creating album video again if already created

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import archivedownloader
 import videocreator
+import youtubeuploader
 
 with open("albumList.txt") as albumList:
+    youtube = youtubeuploader.get_authenticated_service()
     for album in albumList:
         archivedownloader.downloadAlbum(album)
         videocreator.createAlbumVideo(album)
+        youtubeuploader.uploadAlbum(youtube, album)
--- videocreator.py
+++ videocreator.py
@@ -10,14 +10,19 @@ from moviepy import (
 from pathlib import Path
 
 def createAlbumVideo(id):
-    p = Path(id)
+    existVideo = Path(id + ".mp4").is_file()
+    
+    if existVideo == False:
+        p = Path(id)
 
-    tracks = []
-    for audio in p.glob("*.flac"):
-        tracks.append(AudioFileClip(audio))
-    finalTrack = concatenate_audioclips(tracks)
-    finalVideo = ImageClip("./" + id + "/cover_itemimage.jpg", duration=finalTrack.duration)
-    finalVideo = finalVideo.with_audio(finalTrack)
-    finalVideo.write_videofile(id + ".mp4", fps=30)
+        tracks = []
+        for audio in p.glob("*.flac"):
+            tracks.append(AudioFileClip(audio))
+        finalTrack = concatenate_audioclips(tracks)
+        finalVideo = ImageClip("./" + id + "/cover_itemimage.jpg", duration=finalTrack.duration)
+        finalVideo = finalVideo.with_audio(finalTrack)
+        finalVideo.write_videofile(id + ".mp4", fps=30)
+    else:
+        print("Video Already Exist")
     
     
