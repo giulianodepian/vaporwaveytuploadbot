3d27fef0d5bd72493eafebfb6df0e58654b3c5b5
Giuliano De Pian <giulianodepian97@gmail.con>
2025-07-01 12:42:25 -0300

    Various Changes
    Delete Files after uploading to youtube
    Fixed newline char error

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import archivedownloader
 import videocreator
 import youtubeuploader
+import os
+import shutil
 
 with open("albumList.txt") as albumList:
     youtube = youtubeuploader.get_authenticated_service()
     for album in albumList:
-        archivedownloader.downloadAlbum(album)
-        videocreator.createAlbumVideo(album)
-        youtubeuploader.uploadAlbum(youtube, album)
+        formattedAlbum = album.strip()
+        archivedownloader.downloadAlbum(formattedAlbum)
+        videocreator.createAlbumVideo(formattedAlbum)
+        youtubeuploader.uploadAlbum(youtube, formattedAlbum)
+        shutil.rmtree(formattedAlbum, ignore_errors=True)
+        os.remove(formattedAlbum + ".mp4")
